725c317be3b53e819b1b9d5752306a25f587b410
VeeQubit <vee.cyber369@gmail.com>
2026-07-07 11:12:41 +0530

    Setup Node Express backend

---
+++ backend/config/db.js
@@ -0,0 +1,38 @@
+const mysql = require("mysql2");
+require("dotenv").config();
+
+
+const db = mysql.createConnection({
+
+    host: process.env.DB_HOST,
+
+    user: process.env.DB_USER,
+
+    password: process.env.DB_PASSWORD,
+
+    database: process.env.DB_NAME
+
+});
+
+
+db.connect((error)=>{
+
+
+    if(error){
+
+        console.log("Database connection failed");
+        console.log(error);
+
+    }
+
+    else{
+
+        console.log("MySQL Database Connected Successfully");
+
+    }
+
+
+});
+
+
+module.exports = db;
---
+++ backend/server.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const db = require("./config/db");
+
+
+const app = express();
+
+
+// Middleware
+
+app.use(cors());
+
+app.use(express.json());
+
+
+
+// Testing Route
+
+app.get("/", (req,res)=>{
+
+
+    res.send("Artisan Gallery Backend Running");
+
+
+});
+
+
+
+// PORT
+
+const PORT = process.env.PORT || 5000;
+
+
+
+app.listen(PORT, ()=>{
+
+
+    console.log(`Server running on port ${PORT}`);
+
+
+});
